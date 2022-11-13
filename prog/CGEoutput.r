244022f4a2f317f3dd0010b5958a31eb3f0ce633
shinichirofujimoriKU <fujimori.shinichiro.8a@kyoto-u.ac.jp>
2022-11-14 05:07:02 +0900

    small correction in multi panel figure

--- prog/CGEoutput.r
+++ prog/CGEoutput.r
@@ -234,11 +234,13 @@ funcplotgen <- function(rr,progr){
                          nrow=5,ncol=8,rel_widths =c(1,1,1,1,1,1,1,1),align = "hv")
   ggsave(pp_tfcind, file=paste0(outdir,"byRegion/",rr,"/merge/tfcind.png"), width=30, height=20,limitsize=FALSE)
   #Main indicators
-  pp_main <- plot_grid(allplot_nonleg[["GDP_MER"]],allplot_nonleg[["POP"]],allplot[["Tem_Glo_Mea"]],
-                       allplot_nonleg[["Emi_CO2_Ene_and_Ind_Pro"]],allplot_nonleg[["Emi_CO2"]],allplot_nonleg[["Emi_Kyo_Gas"]],
-                       allplot_nonleg[["Pol_Cos_GDP_Los_rat"]],allplot_nonleg[["Pol_Cos_Cns_Los_rat"]],allplot_nonleg[["Prc_Car"]],
-                       allplot_nonleg[["Pop_Ris_of_Hun"]],allplot_nonleg[["Prc_Prm_Ene_Oil"]],allplot_nonleg[["Prc_Sec_Ene_Ele"]],
-                       nrow=4,rel_widths =c(1,1,1.5),align = "hv")
+  p_legend1 <- gtable::gtable_filter(ggplotGrob(allplot[["GDP_MER"]]), pattern = "guide-box")
+  p_legend2 <- gtable::gtable_filter(ggplotGrob(allplot[["Pol_Cos_GDP_Los_rat"]]), pattern = "guide-box")
+  pp_main <- plot_grid(allplot_nonleg[["GDP_MER"]],allplot_nonleg[["POP"]],allplot_nonleg[["Tem_Glo_Mea"]],p_legend1,
+                       allplot_nonleg[["Emi_CO2_Ene_and_Ind_Pro"]],allplot_nonleg[["Emi_CO2"]],allplot_nonleg[["Emi_Kyo_Gas"]],p_legend1,
+                       allplot_nonleg[["Pol_Cos_GDP_Los_rat"]],allplot_nonleg[["Pol_Cos_Cns_Los_rat"]],allplot_nonleg[["Prc_Car"]],p_legend2,
+                       allplot_nonleg[["Pop_Ris_of_Hun"]],allplot_nonleg[["Prc_Prm_Ene_Oil"]],allplot_nonleg[["Prc_Sec_Ene_Ele"]],p_legend1,
+                       nrow=4,rel_widths =c(1,1,1,0.3),align = "hv")
   ggsave(pp_main, file=paste0(outdir,"byRegion/",rr,"/merge/main.png"), width=15, height=15,limitsize=FALSE)
 
 #Emissions
